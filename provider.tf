f3c965e9eefc01ca198152073066bfc8f94ecaf7
lakshjain1 <lakshjain0498@gmail.com>
2024-05-17 17:36:35 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-laksh"
+    key    = "laksh-demo"
+    region = "us-east-1"
+    dynamodb_table = "laksh-locking"
+  }
+}
